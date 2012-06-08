40006ff4c86e5c9ffc0371e005f05c78282dc6ab
Boris Filipov <obiwonn@gmail.com>
2012-06-08 18:08:54 +0300

    added documentation, added Page syntax sugar and backward compatability to renamed: javascripts, stylesheets, body, registerJavascriptHandler, registerStylesheetHandler

--- Page.js
+++ Page.js
@@ -14,21 +14,21 @@ var Page = function(attributes, options) {
 
   this.packageme = require("packageme");
 
-  if(!this.attributes.body)
-    throw new Error("'body' is missing.\n"+JSON.stringify(this.attributes));
+  if(!this.attributes.body && !this.attributes.content)
+    throw new Error("'body' or 'content' is missing.\n"+JSON.stringify(this.attributes));
 
   var root = this.attributes.root;
-  var body = this.attributes.body;
+  var content = this.attributes.content || this.attributes.body; // for backwards compatability
   var layout = this.attributes.layout;
-  var stylesheets = this.attributes.stylesheets;
-  var javascripts = this.attributes.javascripts;
+  var style = this.attributes.style || this.attributes.stylesheets; // for backwards compatability
+  var code = this.attributes.code || this.attributes.javascripts; // for backwards compatability
   var views = this.attributes.views;
 
   if(!root)
-    root = this.attributes.root = path.dirname(body);
+    root = this.attributes.root = path.dirname(content);
 
-  if(body.indexOf(".") == 0 || body.indexOf("/") != 0)
-    this.attributes.body = body = path.normalize(root+"/"+body);
+  if(content.indexOf(".") == 0 || content.indexOf("/") != 0)
+    this.attributes.content = content = path.normalize(root+"/"+content);
   
   
   if(typeof layout == "string" && 
@@ -43,10 +43,10 @@ var Page = function(attributes, options) {
       return v;
   };
 
-  if(stylesheets)
-    this.attributes.stylesheets = stylesheets = _.map(stylesheets, prependRoot);
-  if(javascripts)
-    this.attributes.javascripts = javascripts = _.map(javascripts, prependRoot);
+  if(style)
+    this.attributes.style = style = _.map(style, prependRoot);
+  if(code)
+    this.attributes.code = code = _.map(code, prependRoot);
   if(views)
     this.attributes.views = views = _.map(views, prependRoot);
 }
@@ -55,32 +55,32 @@ _.extend(Page.prototype, Backbone.Events, {
 
   initialize: function() {},
 
-  registerStylesheetHandler : function(req, res, next){
+  renderStyle : function(req, res, next){
     // in case only one argument is given, assume it is app and register handler
     if(arguments.length == 1) {
       var page = this;
       req.get("/" + this.cid + ".css", function(req, res, next){
-        page.registerStylesheetHandler(req, res, next);
+        page.renderStyle(req, res, next);
       });
       return;
     }
 
     res.header("content-type","text/css");
-    this.packageme({source: this.attributes.stylesheets, format: "css"}).pipe(res);
+    this.packageme({source: this.attributes.style, format: "css"}).pipe(res);
   },
 
-  registerJavascriptHandler : function(req, res, next) {
+  renderCode: function(req, res, next) {
     // in case only one argument is given, assume it is app and register handler
     if(arguments.length == 1) {
       var page = this;
       req.get("/"+ this.cid + ".js", function(req, res, next){
-        page.registerJavascriptHandler(req, res, next);
+        page.renderCode(req, res, next);
       });
       return;
     }
 
     res.header("content-type","text/javascript");
-    this.packageme({source: this.attributes.javascripts, format: "js"}).pipe(res);
+    this.packageme({source: this.attributes.code, format: "js"}).pipe(res);
   },
 
   compileViews : function(callback) {
@@ -102,18 +102,22 @@ _.extend(Page.prototype, Backbone.Events, {
     renderData.params = JSON.stringify(req.params ? req.params : {});
     renderData.query = JSON.stringify(req.query ? req.query : {});
 
-    // provide stylesheets and javascripts to templates
-    if(this.attributes.stylesheets)
+    // provide style and code to templates
+    if(this.attributes.style)
       renderData.stylesheets = '<link rel="stylesheet" href="/'+ this.cid + '.css">';
-    if(this.attributes.javascripts)
+    else
+      renderData.stylesheets = undefined;
+    if(this.attributes.code)
       renderData.javascripts = '<script type="text/javascript" src="/'+ this.cid + '.js"></script>';
+    else
+      renderData.javascripts = undefined;
 
     // compile the this and render the output
     var page = this;
     this.compileViews(function(viewsData) {
       renderData.views = viewsData;
       
-      res.render(page.attributes.body, renderData, function(err, bodyData){
+      res.render(page.attributes.content, renderData, function(err, bodyData){
         
         if(page.attributes.layout) {
           renderData.content = bodyData;
@@ -128,6 +132,10 @@ _.extend(Page.prototype, Backbone.Events, {
   }
 
 });
+
+// backward compatability
+Page.prototype.registerStylesheetHandler = Page.prototype.renderStyle;
+Page.prototype.registerJavascriptHandler = Page.prototype.renderCode;
 
 Page.extend = Backbone.View.extend;
 module.exports = Page;
--- README.md
+++ README.md
@@ -1,2 +1,111 @@
 expressSite
 ===========
+"Addon" nodejs module for expressjs framework providing lightweight 'pages' support, or other called client-side app single-page apps.
+
+Includes:
+- packageme (https://github.com/camplight/packageme/) for packaging directories of files needed by client-side apps
+- Backbone-like class implementation of client-side app (a Page).
+- consolidate.js for server-side template engines 
+
+# Usage #
+
+add "expressSite" to dependencies in package.json
+add your prefered template engine, for example "jade" to dependencies in package.json
+add "consolidate" to dependencies in package.json
+
+Create `app.js` with the following example code:
+
+    var app = require("expressSite");
+
+    var cons = require("consolidate");
+    app.engine('jade', cons.jade);
+
+    app.set('view engine', 'jade');
+
+    app.configure(function(){
+      app.useExpressSiteMiddleware();
+    });
+
+    app.defaultPageAttributes({
+      root: __dirname+"/client",
+      layout: "./templates/layout.jade",
+    });
+
+    app.addPage({
+      url: "/",
+      content: "./templates/index",
+      variables: {
+        title: "Index Page"
+      }
+    });
+
+    app.listen(8000, function(){
+      console.log("listening on 8000");
+    });
+
+Create `client` and `client/templates` folders.
+
+Place your layout template containing these as minimum at `/client/templates/layout.jade`:
+
+    !!! html
+    head
+      !{javascripts}
+      !{stylesheets}
+    body
+      !{views}
+      !{content}
+
+`{javascripts}`, `{stylesheets}` & `{views}` will be replaced by expressSite with appropiate path to packaged javascripts, stylesheets & views.
+`{content}` will be replaced with the content of the pages using that layout.
+
+Create `/client/templates/index.jade` file with content as you like.
+
+Launch the application or refer to the demo for additional usage examples.
+
+# Reference #
+## expressSite ##
+- `addPage`(`Page Class` or `Page attributes object`)
+- `defaultPageAttributes`(`Page attributes object`)
+
+Note that Page attributes which are Array(`views`, `style`, `code`) or Object(`variables`) and are present in `defaultPageAtrributes` will be appened/merged with the `defaultPageAttributes`. However `Page.defaults` will override those arrays/objects.
+
+## Page ##
+
+    var Page = require("expressSite/Page");
+
+    var MyPage = module.exports = Page.extend({
+      // attributes
+      defaults: {
+        root: String,
+        url: String,
+        code: Array[String] or String,
+        views: Array[String] or String,
+        style: Array[String] or String,
+        content: String,
+        variables: Object
+      }
+
+      // methods
+      initialize: function(){ ... },
+      renderCode: function(app || req, res, next) { ... },
+      renderStyle: function(app || req, res, next) { ... },
+      compileViews: function(callback(data)) { ... },
+      render: function(app || req, res, next) { ... }
+    });
+
+### attributes ###
+Note that all paths except `root` can be full or relative(starting with `./`, `../` or without `/`), in case of relative usage, `root` value will be prepended. 
+
+- `root` -> full path to page's assets (code, styles, templates, views), if missing content/body parent directory will be used as root.
+- `content` or `body` -> path to template file to be rendered /must be provided/
+- `layout` -> path to template file to be used as layout, if missing only the content/body will be rendered and send as response.
+- `url` -> uri to which `expressSite.addPage` should mount given page, if missing `addPage` won't mount the page in express router.
+- `code` or `javascripts` -> path or array of paths to page's code/javascripts/coffeescript & etc. It is mainly been passed directly to `packageme`. `Page.renderCode(app)` is magically used by `expressSite.addPage` to register route handler for those code assets.
+- `style` or `stylesheets` -> path or array of paths to page's stylesheets. It is again send directly to `packageme`. And there is `Page.renderStyle(app)` for providing packaged version of those assets.
+- `views` -> array of paths to page's client-side templates. Folders or Files which `packageme` will combine and inject to the page's content within `views` local variable. Those views/templates can be used within browser and as such to minify client requests to the site.
+- `variables` -> object of variables which will be used as data source when rendering the page's templated content.
+
+## Page.renderCode(app) ##
+## Page.renderStyle(app) ##
+## Page.render(req, res, next) ##
+## Page.initialize(attributes, options) ##
